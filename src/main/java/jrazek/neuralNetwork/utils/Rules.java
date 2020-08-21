3ed73f69b68b4a6d8a66861553628099e158fda4
Jakub <jakub.jakubecky123@gmail.com>
2020-08-21 19:58:03 +0200

    compiles

--- src/main/java/jrazek/neuralNetwork/utils/Rules.java
+++ src/main/java/jrazek/neuralNetwork/utils/Rules.java
@@ -1,7 +1,7 @@
 package jrazek.neuralNetwork.utils;
 
 public class Rules {
-    public static final int layersNum = 4;
+    public static final int layersNum = 3;
     public static final int inputNeurons = 2;
     public static final int hiddenNeurons = 2;
     public static final int outputNeurons = 2;
