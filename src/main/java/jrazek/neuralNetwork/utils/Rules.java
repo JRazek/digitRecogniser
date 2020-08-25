3b4d24d6c1bfa143851780e0df7cc23c30a1c24c
Jakub <jakub.jakubecky123@gmail.com>
2020-08-25 16:17:58 +0200

    just config

--- src/main/java/jrazek/neuralNetwork/utils/Rules.java
+++ src/main/java/jrazek/neuralNetwork/utils/Rules.java
@@ -1,10 +1,11 @@
 package jrazek.neuralNetwork.utils;
 
 public class Rules {
-    public static final int layersNum = 4;
-    public static final int inputNeurons = 2;
-    public static final int hiddenNeurons = 2;
-    public static final int outputNeurons = 2;
+    public static final int layersNum = 6;
+    public static final int inputNeurons = 10;
+    public static final int hiddenNeurons = 10;
+    public static final int outputNeurons = 8;
     public static final double gradientDescentRate = 1;
-    public static final int iterations = 1000;
+    public static final int iterations = 100;
+    public static boolean showErrorEveryIteration = true;
 }
