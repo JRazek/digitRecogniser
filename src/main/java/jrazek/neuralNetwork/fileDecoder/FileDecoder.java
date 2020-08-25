13e85af4553dd2312c5be9b3f300f00d356e701d
JRazek <jakub.jakubecky123@gmail.com>
2020-08-25 23:22:07 +0200

    reader

--- src/main/java/jrazek/neuralNetwork/fileDecoder/FileDecoder.java
+++ src/main/java/jrazek/neuralNetwork/fileDecoder/FileDecoder.java
@@ -1,7 +1,9 @@
 package jrazek.neuralNetwork.fileDecoder;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 public class FileDecoder {
     private String navigationFile = "dataSet/training-a.csv";
@@ -24,8 +26,16 @@ public class FileDecoder {
 
         return null;
     }
-    public HandWrittenNumber getNextImage(){
-       readNext().split(",");
+    public HandWrittenNumber getNextImage() throws IOException {
+        try {
+            String [] data = Objects.requireNonNull(readNext()).split(",");
+            BufferedImage img = FileReader.readImageFile(datasetFolder + data[0]);
+            int digit = Integer.parseInt(data[3]);
+            return new HandWrittenNumber(img, digit);
+        }catch (NumberFormatException e){
+            e.printStackTrace();
+        }
+
        //filename,original filename,scanid,digit,database name original,contributing team,database name
         //a00000.png,Scan_58_digit_5_num_8.png,58,5,BHDDB,Buet_Broncos,training-a
         return null;
