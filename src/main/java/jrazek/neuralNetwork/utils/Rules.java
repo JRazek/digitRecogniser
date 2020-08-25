03c353c599ee100c5505bbb9710446fbc5eba530
Jakub <jakub.jakubecky123@gmail.com>
2020-08-25 15:33:18 +0200

    bias left to go

--- src/main/java/jrazek/neuralNetwork/utils/Rules.java
+++ src/main/java/jrazek/neuralNetwork/utils/Rules.java
@@ -1,10 +1,10 @@
 package jrazek.neuralNetwork.utils;
 
 public class Rules {
-    public static final int layersNum = 3;
+    public static final int layersNum = 4;
     public static final int inputNeurons = 2;
     public static final int hiddenNeurons = 2;
     public static final int outputNeurons = 2;
-    public static final double gradientDescentRate = 0.1;
+    public static final double gradientDescentRate = 1;
     public static final int iterations = 1000;
 }
