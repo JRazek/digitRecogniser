9094873cb4a29847ff866d2925b57ce92e0e06be
Jakub <jakub.jakubecky123@gmail.com>
2020-08-26 10:34:14 +0200

    rule

--- src/main/java/jrazek/neuralNetwork/utils/Rules.java
+++ src/main/java/jrazek/neuralNetwork/utils/Rules.java
@@ -5,7 +5,7 @@ public class Rules {
     public static final int inputNeurons = 1024;
     public static final int hiddenNeurons = 16;
     public static final int outputNeurons = 10;
-    public static final double gradientDescentRate = 10;
+    public static final double gradientDescentRate = 0.1;
     public static final int iterations = 1000000;
     public static boolean showErrorEveryIteration = true;
     public static int updateRateAverage = 10;//every 10 images the gradient is averaged and then changed
